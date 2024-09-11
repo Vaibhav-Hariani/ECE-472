b1461e06230e8edafcee14fa037091edd87b130a
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-09-11 11:56:27 -0400

    refactored proj1 folder. Added new file to write custom tests, and restructured linear so that basis expansion is a separate module

--- Proj_1/basis_expansion.py
+++ Proj_1/basis_expansion.py
@@ -13,34 +13,54 @@ class Linear(tf.Module):
             name="Linear/w",
         )
 
-        ## Trainable mew & sigmas
-        self.mew = tf.Variable(
-            rng.normal(shape=[num_inputs, num_outputs], stddev=stddev),
-            trainable=True,
-            name="Linear/mew",
-        ) 
-        self.sig = tf.Variable(
-            rng.normal(shape=[num_inputs, num_outputs], stddev=stddev),
-            trainable=True,
-            name="Linear/signal",
-        ) 
-
+        # Does not make sense looking at the data, but leaving it in
         self.bias = bias
         if self.bias:
             self.b = tf.Variable(
-                tf.zeros(
-                    shape=[1, num_outputs],
-                ),
+                tf.zeros(shape=[1, num_outputs]),
                 trainable=True,
                 name="Linear/b",
             )
 
     def __call__(self, x):
-        z = -1 * tf.math.square((x - self.mew) / self.sig)
-        z = tf.math.exp(z)
+        z = x @ self.w
+
         if self.bias:
             z += self.b
+
         return z
+
+
+class BasisExpansion(tf.Module):
+
+    def __init__(self, M):
+        self.linear = Linear(M, 1, bias=True)
+
+        ## Trainable mu & sigmas
+        self.mu = tf.Variable(
+            rng.normal(shape=[1, M]),
+            trainable=True,
+            name="BasisExpansion/mu",
+        )
+
+        self.sig = tf.Variable(
+            rng.normal(shape=[1, M]),
+            trainable=True,
+            name="BasisExpansion/sigma",
+        )
+
+    def __call__(self, x):
+        ## This computes the PHI function
+        phi = -1 * tf.math.square((x - self.mu) / self.sig)
+        phi = tf.math.exp(phi)
+        # Multiplies by weights for w
+        z = self.linear(phi)
+        return z
+    
+    ##Returns basis at a given index, for a set of values x
+    def sliver_call(self, index,x):
+        phi = -1 * tf.math.square((x - self.mu[0,index]) / self.sig[0,index])
+        return tf.math.exp(phi)
 
 
 def grad_update(step_size, variables, grads):
@@ -63,8 +83,7 @@ if __name__ == "__main__":
         description="Fits a linear model to some data, given a config",
     )
 
-    parser.add_argument("-c", "--config", type=Path,
-                        default=Path("config.yaml"))
+    parser.add_argument("-c", "--config", type=Path, default=Path("config.yaml"))
     args = parser.parse_args()
 
     config = yaml.safe_load(args.config.read_text())
@@ -73,32 +92,23 @@ if __name__ == "__main__":
     rng.reset_from_seed(0x43966E87BD57227011B5B03B58785EC1)
 
     num_samples = config["data"]["num_samples"]
-    num_inputs = 1
-    num_outputs = 1
+    M = 10
 
-    # Creates an array of num_samples randomly spaced x coordinates
-    # b is the bias.
-    x = rng.uniform(shape=(num_samples, num_inputs))
+    #
+    x = rng.uniform(shape=(num_samples, 1))
 
-    w = rng.normal(shape=(num_inputs, num_outputs))
-    ###
-    b = rng.normal(shape=(1, num_outputs))
-
-
-# Corresponding y values for all the x values.
     y = rng.normal(
-        shape=(num_samples, num_outputs),
-        mean=tf.sin(2*3.14159 * x),
+        shape=(num_samples, 1),
+        mean=tf.sin(2 * 3.14159 * x),
         stddev=config["data"]["noise_stddev"],
     )
 
-    linear = Linear(num_inputs, num_outputs)
+    basis = BasisExpansion(M)
 
     num_iters = config["learning"]["num_iters"]
     step_size = config["learning"]["step_size"]
     decay_rate = config["learning"]["decay_rate"]
     batch_size = config["learning"]["batch_size"]
-
     refresh_rate = config["display"]["refresh_rate"]
 
     bar = trange(num_iters)
@@ -111,36 +121,40 @@ if __name__ == "__main__":
             x_batch = tf.gather(x, batch_indices)
             y_batch = tf.gather(y, batch_indices)
 
-            y_hat = linear(x_batch)
-            loss = tf.math.reduce_mean((y_batch - y_hat) ** 2)
+            y_hat = basis(x_batch)
+            loss = tf.math.reduce_mean((y_batch - y_hat) ** 2 / 2)
 
-        grads = tape.gradient(loss, linear.trainable_variables)
-        grad_update(step_size, linear.trainable_variables, grads)
+        grads = tape.gradient(loss, basis.trainable_variables)
+        grad_update(step_size, basis.trainable_variables, grads)
 
         step_size *= decay_rate
+
         if i % refresh_rate == (refresh_rate - 1):
             bar.set_description(
                 f"Step {i}; Loss => {loss.numpy():0.4f}, step_size => {step_size:0.4f}"
             )
             bar.refresh()
 
+    fig, (ax1,ax2) = plt.subplots(1,2)
 
-
-    ## Render the linear graph
-    fig, ax = plt.subplots()
-    ax.plot(x.numpy().squeeze(), y.numpy().squeeze(), "x")
+    ax1.plot(x.numpy().squeeze(), y.numpy().squeeze(), "x")
     a = tf.linspace(tf.reduce_min(x), tf.reduce_max(x), 100)[:, tf.newaxis]
-    ax.plot(a.numpy().squeeze(), linear(a).numpy().squeeze(), "-")
-#    ax.plot(a.numpy().squeeze(), tf.math.sin(a).numpy().squeeze(), linestyle="dashed")
-    sinx = tf.linspace(0,1,100)
-    siny = tf.math.sin(2*3.14159*sinx)
-    ax.plot(sinx.numpy().squeeze(), siny.numpy().squeeze(), linestyle="dashed")
-
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_title("Linear fit using SGD")
-
-    h = ax.set_ylabel("y", labelpad=10)
+    ax1.plot(a.numpy().squeeze(), basis(a).numpy().squeeze(),linestyle="dashed")
+    sinx = tf.linspace(0, 1, 100)
+    siny = tf.math.sin(2 * 3.14159 * sinx)
+    ax1.plot(sinx.numpy().squeeze(), siny.numpy().squeeze())
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    ax1.set_title("Fig1")
+    h = ax1.set_ylabel("y", labelpad=10)
     h.set_rotation(0)
+
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+    ax2.set_title("Fig1")
+    for i in range(0,M):
+        ax2.plot(a.numpy().squeeze(), basis.sliver_call(i,a).numpy().squeeze())
+
+
 
     fig.savefig("plot.pdf")
