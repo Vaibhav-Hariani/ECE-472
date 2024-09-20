7792f75aacd0be2e6d658ae6899e66cdafff6ff6
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-09-20 12:42:27 -0400

    Modified Gitignore with MNIST data.

--- Proj_2/adam.py
+++ Proj_2/adam.py
@@ -7,21 +7,42 @@ class Adam():
         beta_2=0.999,
         step_size = 0.001,
         epsilon = 1e-8,
+        w = 0
     ):
         self.beta_1 = beta_1
         self.beta_2 = beta_2
         self.step_size = step_size
         self.epsilon = epsilon
         self.t = 0
+        self.w = w
         self.size = size
         self.m_ts = [0] * size
         self.v_ts = [0] * size
 
-    def train(self,grads,vars):
+    def train(self,grads,vars,adamW=False):
         self.t += 1
         for i in range(self.size):
-            self.m_ts[i] = self.beta_1 * self.m_ts[i] + (1-self.beta_1) * grads[i]
-            self.v_ts[i] = self.beta_2 * self.v_ts[i] + (1-self.beta_2) * (grads[i] * grads[i])
+            grad = grads[i]
+            if not adamW:
+                grad += vars[i] * self.w
+            
+            self.m_ts[i] = self.beta_1 * self.m_ts[i] + (1-self.beta_1) * grad
+            self.v_ts[i] = self.beta_2 * self.v_ts[i] + (1-self.beta_2) * (grad * grad)
             self.step_size = self.step_size * math.sqrt(1-self.beta_2**self.t)/(1-self.beta_1**self.t)
-            offset = (self.step_size * self.m_ts[i] / math.sqrt(self.v_ts[i] + self.epsilon))
+            epsilon = self.epsilon / math.sqrt(1-self.beta_2**self.t)
+            offset = (self.step_size * self.m_ts[i] / math.sqrt(self.v_ts[i] + epsilon))
+            if adamW:
+                offset+= self.w * vars[i] 
             vars[i].assign_sub(offset)
+    
+    def AdaMax(self,grads,vars):
+        self.t += 1
+        for i in range(self.size):
+            grad = grads[i] + vars[i] * self.w
+            self.m_ts[i] = self.beta_1 * self.m_ts[i] + (1-self.beta_1) * grad
+            #For AdaMax, v_t becomes u_t according to the paper 
+            self.u_ts[i] = math.max(self.beta_2 * self.v_ts[i], math.abs(grad)),
+            offset = (self.step_size / (1 - self.beta_1**self.t)) * self.m_ts[i] / self.u_ts[i]
+            vars[i].assign_sub(offset)
+
+
