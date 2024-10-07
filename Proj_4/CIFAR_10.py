59f765ef3fb8ff2d222518241e356f8541f3f54a
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-10-06 23:43:51 -0400

    "pushing to desktop"

---
+++ Proj_4/CIFAR_10.py
@@ -0,0 +1,132 @@
+import math
+
+import tensorflow as tf
+from conv2d import Conv2d
+from MLP import MLP
+
+class GroupNorm(tf.Module):
+    def __init__(self,groups,channels, eps=1e-5, seed = 42):
+        rng = tf.random.get_global_generator()
+        rng.reset_from_seed(seed)
+        stddev = 1/ channels
+        self.gamma = tf.Variable(
+            rng.normal(shape=[1,channels,1,1], stddev=stddev),
+            trainable=True,
+            name="GN/gamma",
+        )
+        self.beta = tf.Variable(
+            rng.normal(shape=[1,channels,1,1], stddev=stddev),
+            trainable=True,
+            name="GN/beta",
+        )
+        self.eps = eps
+        self.groups = groups
+    
+    ##Directly from the paper
+    def __call__(self,x):
+        N,C,H,W = x.shape
+        #C = channels
+        x = tf.reshape(x, [N, self.groups, C // self.groups, H, W])
+        mean, var = tf.nn.moments(x, [2,3,4], keep_dims=True)
+        x = (x - mean) / tf.sqrt(var + self.eps)
+        x = tf.reshape(x, [N,C,H,W])
+        return x * self.gamma + self.beta
+
+
+
+class ResidualBlock(tf.Module):
+    def __init__(self,dim,channels, groups,activation,dropout_rate=0.2, layers_between=2):
+        self.GN = GroupNorm(groups=groups,channels=channels)
+        self.convs = []
+        for layer in range(layers_between): 
+            self.convs.append(Conv2d(dim=dim,in_channel=3,out_channel=3,dropout_rate=dropout_rate))
+        self.activation = activation
+
+    def __call__(self,input,dropout=False):
+        ##Using this to prevent soft copying
+        intermediate = tf.identity(input)
+        for layer in self.convs :
+            intermediate = self.GN(intermediate)
+            intermediate = self.activation(intermediate)
+            intermediate = layer(intermediate,dropout)
+        return input + intermediate
+
+class Classifier(tf.Module):
+    def __init__(
+        self,
+        input_dims,
+        input_channels,
+        output_dim,
+        pool_dims=1,
+        conv_dims=5,
+        num_res=4,
+        num_lin_layers=5,
+        hidden_lin_width=125,
+        lin_activation=tf.identity,
+        lin_output_activation=tf.identity,
+        dropout_rate=0.1,
+    ):
+        self.input_dims = input_dims
+        self.res_layers = []
+
+        for res_layer in range(0, num_res):
+            self.res_layers.append(ResidualBlock(dim=conv_dims,channels=input_channels,dropout_rate=dropout_rate))
+        
+        self.pool_kernel = [1,pool_dims,pool_dims,1]
+        self.strides = [1,pool_dims,pool_dims,1]
+        self.padding='SAME'
+
+        self.output_perceptron = MLP(
+            num_inputs=input_dims**(2*input_channels)/(pool_dims**2),
+            num_outputs=output_dim,
+            num_hidden_layers=num_lin_layers,
+            hidden_layer_width=hidden_lin_width,
+            hidden_activation=lin_activation,
+            output_activation=lin_output_activation,
+            dropout_rate=dropout_rate,
+        )
+
+    def __call__(self, input, dropout=False):
+        current = input
+        for layer in self.res_layers:
+            current = layer(current, dropout)
+
+        # Define the max pooling operation
+
+        pooled_out = tf.nn.max_pool2d(current,ksize=self.pool_kernel,strides=self.size,padding=self.padding)
+        perceptron_in = tf.reshape(pooled_out, (pooled_out.shape[0], -1))
+        
+        ##Flattens for perceptron
+        return self.output_perceptron(perceptron_in, dropout)
+
+
+if __name__ == "__main__":
+    import os
+    import numpy as np
+    from CIFAR_UTILS import unpickle, augment
+    from tqdm import trange
+
+    # Getting training data from local CIFAR
+    CIFAR_LOC = "CIFAR"
+    CIFAR_FOLDER= "cifar-10-batches-py"
+    IMG_DIMS=(-1,3,32,32)
+
+    batches = ["data_batch_" + str(x+1) for x in range(1)]
+    label_strings = unpickle(os.path.join(CIFAR_LOC,CIFAR_FOLDER, "batches.meta"))[b'label_names']
+    images = []
+    labels = []
+    for batch in batches:
+        path = os.path.join(CIFAR_LOC, CIFAR_FOLDER,batch)
+        raw_dict = unpickle(path)
+        batch_images = np.reshape(raw_dict[b'data'], IMG_DIMS)
+        batch_images = np.transpose(batch_images,(0,2,3,1))        
+        images.append(batch_images)
+        ##Images are subject to gaussian noise, inversion, and flipping in two dimensions
+        extra_images = augment(batch_images)
+        images.append(extra_images)
+        labels.append(raw_dict[b'labels']) 
+    image_set = np.concatenate(images, axis=0)
+    labels=np.concatenate(images,axis=0)
+
+    ## And now, the training
+    
