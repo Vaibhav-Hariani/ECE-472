3ced2f324fca1ce57403455b75c0d0a621e6876b
vaibhav.hariani <vaibhav.hariani@cooper.edu>
2024-11-01 14:04:25 -0400

    Started creating transformer structure.

---
+++ Proj_6/Transformer.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+
+class MultiHeadAttention(tf.Module):
+    
+    def dot_product_attention():
+        pass
+
+    def __call__():
+        pass
+        
+#Generates Embeddings & Positional Encodings 
+#Given an input
+class Encoder(tf.Module):
+
+    def tokenizer():
+        pass
+
+
+    def __call__():
+        pass
+
+
+class Transformer(tf.Module):
+    def __call__():
+        pass
+
+
+
+
