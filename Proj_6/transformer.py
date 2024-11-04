9f20e625846488cab1d5b7963b879fd13d0b667d
vaibhav.hariani <vaibhav.hariani@cooper.edu>
2024-11-03 23:25:07 -0500

    maybe the transformer is done now? Finished a first pass, super basic encoder. Will likely need to completely rewrite it.

--- Proj_6/transformer.py
+++ Proj_6/transformer.py
@@ -4,20 +4,30 @@ from resnet_linear import ResidualBlock
 import numpy as np
 
 class Pre_Processor(tf.Module):
-    ##This takes in an input string, tokenizes it
-    ##Places them in an embedding space,
-    ##And then handles positional encoding.
-    def __init__(self, embed_space,n,d):
+    def __init__(self, corpus,n,d):
+        
         self.n = n
-        self.d
-        pass
+        self.d = d
+
+        cur_words = 0
+        self.map_of_words = {}
+        ##A first pass attempt at building an embedding space given a corpus
+        ##This has the limitation of not being able to parse tokens not in the corpus
+        for line in corpus:
+            for word in str.split(line):
+                if word not in self.map_of_words:
+                    self.map_of_words[word] = cur_words
+                    cur_words += 1
+
+        self.embeddings_size = cur_words
     
 
     ##This is cursed to read
     ##But Numpy is so powerful
     def positional(self, seq):
         ##The number of elements in the sequence
-        ##Assuming it's formatting as nxE
+        ##Assuming it's formatting as nxE: n is number of sentences
+        ## X is encoding space
 
         index_row = np.arange(seq.shape[0])
         ##For length of embedding space
@@ -37,14 +47,23 @@ class Pre_Processor(tf.Module):
         term[1::2,] = np.cos(term[1::2,])
         return term
 
-
+    ##This takes in an input string, tokenizes it
+    ##Places them in an embedding space,
+    ##And then handles positional encoding.
     def __call__(self, context):
-        ##Given context: Break it up into tokens     
-        tokens = np.array(str.split(context))
 
-        embedded_tokens = None
-        ##Assuming that tokens get properly parsed here
-        return self.positional(embedded_tokens)
+        ##Given context: Break it up into tokens     
+        tokens = str.split(context)
+        
+        embeddings = np.zeros(self.embeddings_size)
+
+        ##Assuming that we've seen almost everything        
+        for token in tokens:
+            if token in self.map_of_words:
+                index = self.map_of_words[token]
+                embeddings[index] = 1
+
+        return self.positional(embeddings)
 
 
 
